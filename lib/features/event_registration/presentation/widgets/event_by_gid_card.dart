99a6aa4ec8792358a69a36e0b0aa725109df3e73
Prabhat Jana Official <itsmeprabhatjana@gmail.com>
2025-04-23 02:33:10 +0530

    feat: refactor EventByGidCard to use EventByGidCardTiles for improved layout and styling

--- lib/features/event_registration/presentation/widgets/event_by_gid_card.dart
+++ lib/features/event_registration/presentation/widgets/event_by_gid_card.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
-import 'package:megatronix/common/widgets/custom_richtext.dart';
 import 'package:megatronix/features/event_registration/domain/entities/event_registration_details_entity.dart';
+import 'package:megatronix/features/event_registration/presentation/widgets/event_by_gid_card_tiles.dart';
 import 'package:megatronix/theme/app_theme.dart';
 
 class EventByGidCard extends StatefulWidget {
@@ -27,7 +27,7 @@ class _EventByGidCardState extends State<EventByGidCard> {
   Widget build(BuildContext context) {
     final size = MediaQuery.sizeOf(context);
     return Container(
-      height: 275,
+      height: 331,
       width: size.width,
       alignment: Alignment.center,
       padding: EdgeInsets.all(10),
@@ -36,54 +36,62 @@ class _EventByGidCardState extends State<EventByGidCard> {
         color: AppTheme.scaffoldBackgroundColor,
         borderRadius: BorderRadius.circular(25),
       ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(15),
+        child: CupertinoListSection.insetGrouped(
+          margin: const EdgeInsets.all(5),
+          backgroundColor: AppTheme.darkBackground,
+          separatorColor: AppTheme.dividerColor,
+          dividerMargin: -40,
+          decoration: BoxDecoration(
+            color: AppTheme.blackBackground,
+            borderRadius: BorderRadius.circular(15),
+          ),
+          header: Text(
             '${widget.index + 1}. ${widget.eventByGidEntity.eventName}',
             style: TextStyle(
-              color: AppTheme.primaryBlueAccentColor,
-              fontSize: 22,
-              fontWeight: FontWeight.bold,
+              color: AppTheme.whiteBackground,
             ),
           ),
-          CustomRichtext(
-            title: '- Team Name',
-            subtitle: widget.eventByGidEntity.teamName,
-          ),
-          CustomRichtext(
-            title: '- Contacts',
-            subtitle:
-                widget.eventByGidEntity.contacts.map((e) => e.name).join(", "),
-          ),
-          CustomRichtext(
-            title: '- TID',
-            subtitle: widget.eventByGidEntity.tid,
-          ),
-          CustomRichtext(
-            title: '- GID List',
-            subtitle: '\n${widget.eventByGidEntity.gidList.join("\n")}',
-          ),
-          CustomRichtext(
-            title: '- Played Status',
-            subtitle:
-                widget.eventByGidEntity.hasPlayed ? 'Played' : 'Not Played',
-          ),
-          CustomRichtext(
-            title: '- Paid Status',
-            subtitle: widget.eventByGidEntity.paid ? 'Paid' : 'Not Paid',
-          ),
-          CustomRichtext(
-            title: '- Qualified Status',
-            subtitle: widget.eventByGidEntity.qualified
-                ? 'Qualified'
-                : 'Not Qualified',
-          ),
-          CustomRichtext(
-            title: '- Position',
-            subtitle: widget.eventByGidEntity.position,
-          ),
-        ],
+          children: [
+            EventByGidCardTiles(
+              leading: 'Team Name',
+              title: widget.eventByGidEntity.teamName,
+            ),
+            EventByGidCardTiles(
+              leading: 'Contacts',
+              title: widget.eventByGidEntity.contacts
+                  .map((e) => e.name)
+                  .join(", "),
+            ),
+            EventByGidCardTiles(
+              leading: 'TID',
+              title: widget.eventByGidEntity.tid,
+            ),
+            EventByGidCardTiles(
+              leading: 'GIDs',
+              title: widget.eventByGidEntity.gidList,
+              isGID: true,
+            ),
+            EventByGidCardTiles(
+              leading: 'Played Status',
+              title:
+                  widget.eventByGidEntity.hasPlayed ? 'Played' : 'Not Played',
+              isTrue: true,
+            ),
+            EventByGidCardTiles(
+              leading: 'Qualified Status',
+              title: widget.eventByGidEntity.qualified
+                  ? 'Qualified'
+                  : 'Not Qualified',
+              isTrue: true,
+            ),
+            EventByGidCardTiles(
+              leading: 'Position',
+              title: widget.eventByGidEntity.position,
+            ),
+          ],
+        ),
       ),
     );
   }
